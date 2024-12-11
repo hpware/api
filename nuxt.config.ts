aa6bcb2e2b29d92afd38f23c0d4165c158919ffd
吳元皓 <hpware3@gmail.com>
2024-12-11 19:04:52 +0800

    Refactor nuxt.config.ts to update route rules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,11 +2,18 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/robots'],
+  modules: ['@nuxtjs/robots', 'nuxt-umami'],
 
   routeRules: {
     '/': {prerender: true},
-    '/app/**': {ssr: true},
-    '/api/**': {ssr:true},
-  }
+    '/app/**': {cors: true},
+    '/api/**': {cors:true},
+  },
+  umami: {
+    enabled: true,
+    host: 'https://data.yuanhau.com',
+    autoTrack: true,
+    id: '7add4cf7-307d-494c-8dbb-81fd5c9615ac',
+    ignoreLocalhost: true,
+  },
 })
