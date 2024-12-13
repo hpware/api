debdaac04097d58ca32c9788419b9763dc917459
Howard <hw@yuanhau.com>
2024-12-14 06:52:23 +0800

    Refactor error handling in AI Yue Wu Christmas theme API and add favicon

--- server/api/events/ai-yue-wu-2024-chrismas-theme-page-list.ts
+++ server/api/events/ai-yue-wu-2024-chrismas-theme-page-list.ts
@@ -7,13 +7,15 @@ export default defineEventHandler(async (event) => {
             const {data} = await supabase.from("lovemusic_202412").select();
             return data;
         } catch (e) {
-            return {
-                'err': e
-            };
+            throw createError({
+                statusCode: 500,
+                statusMessage: '伺服器或資料庫錯誤!'
+              })
         }
     } else {
-        return {
-            'err': 403
-        }
+        throw createError({
+            statusCode: 403,
+            statusMessage: '服務無法被存取!'
+          })
     }
 })
