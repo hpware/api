978a5c77e5c7553df09da30a16090b033af80ea5
flash_user <hw@yuanhau.com>
2024-12-17 10:37:12 +0800

    Refactor bus API endpoints to use a constant URL for fetching estimated arrival times and simplify response structure

--- server/api/transport/bus/shilin/508s.ts
+++ server/api/transport/bus/shilin/508s.ts
@@ -1,9 +1,10 @@
 import getToken from "~/server/components/loadTDXToken";
+const url = "https://tdx.transportdata.tw/api/basic/v2/Bus/EstimatedTimeOfArrival/City/Taipei/508%E5%8D%80?%24top=1&%24skip=69&%24format=JSON"
 
 const BUS = async () =>{
     const token = await getToken();
     try {
-        const api = await fetch("https://tdx.transportdata.tw/api/basic/v2/Bus/EstimatedTimeOfArrival/City/Taipei/508%E5%8D%80?%24top=1&%24skip=69&%24format=JSON",
+        const api = await fetch(url,
             {
                headers: {
                     Authorization: `Bearer ${token}`,
@@ -21,12 +22,7 @@ export default defineEventHandler(async (event) =>{
     if (event.node.req.method === 'POST' || event.node.req.method === 'GET') {
         try {
             const data = await BUS();
-            return {
-                bus: {
-                    stopname: data.StopName,
-                    route: data.RouteName,
-                    srcupdatetime: data.SrcUpdateTime
-                }            }
+            return data;
         } catch (e) {
             throw createError({
                 statusCode: 500,
