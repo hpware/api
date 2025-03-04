012b2e51c50634048cf34522959529d8c85ce7ea
吳元皓 <hpware3@gmail.com>
2025-03-05 00:09:36 +0800

    Add new endpoint for old event data retrieval

---
+++ server/api/dumbdata/lmsite-data-old-events.ts
@@ -0,0 +1,44 @@
+export default defineEventHandler((event) => {
+    setHeader(event, "Content-Type", "applicaton/json");
+    return {
+      recentevents: true,
+      events: [
+        {
+          item: 0,
+          name: "2025",
+          date: "2025-05-21",
+          dmImage: "#",
+          link: "data",
+        },
+        {
+          item: 1,
+          name: "20203",
+          date: "2025-02-21",
+          dmImage: "https://images.unsplash.com/photo-1721100152646-1bbfeb581505",
+          link: "#",
+        },
+        {
+          item: 2,
+          name: "20203",
+          date: "2025-02-21",
+          dmImage: "https://images.unsplash.com/photo-1721100152646-1bbfeb581505",
+          link: "#",
+        },
+        {
+          item: 3,
+          name: "20203",
+          date: "2025-02-21",
+          dmImage: "https://images.unsplash.com/photo-1721100152646-1bbfeb581505",
+          link: "#",
+        },
+        {
+          item: 4,
+          name: "20203",
+          date: "2025-02-21",
+          dmImage: "https://images.unsplash.com/photo-1721100152646-1bbfeb581505",
+          link: "#",
+        }
+      ]
+    }
+  });
+  
